e63e28c8edd404657cec5a5ba7ac379f90c9c313
Benjamin Kaylor <blkaylor22@gmail.com>
2024-03-17 08:29:35 -0600

    java functioning finally

--- home/neovim/plugins/nvim-jdtls.nix
+++ home/neovim/plugins/nvim-jdtls.nix
@@ -1,6 +1,10 @@
+{pkgs, ...}:
 {
   programs.nixvim.plugins.nvim-jdtls = {
     enable = true;
-    data = "/home/ben/Code/Mines/SoftwareEngineering-CSCI306/gradleProjects/Clue";
+    cmd = [
+      "${pkgs.jdt-language-server}/bin/jdtls"
+      "-data" "/home/ben/Code/Mines/SoftwareEngineering-CSCI306/gradleProjects/Clue"
+    ];
   };
 }

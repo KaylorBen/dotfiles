3f3e84a1f19434a43374fa9822164c80f50b7e57
Benjamin Kaylor <blkaylor22@gmail.com>
2024-03-19 10:34:34 -0600

    neovim plugins added

--- home/neovim/lua/benix/lspconfig.lua
+++ home/neovim/lua/benix/lspconfig.lua
@@ -17,42 +17,67 @@ local function lsp_keymaps(bufnr)
     keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 end
 
+M.on_attach = function(client, bufnr)
+  lsp_keymaps(bufnr)
+end
+
+function M.common_capabilities()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  return capabilities
+end
+
 function M.config()
-    -- local wk = require "which-key"
-    
-    local lspconfig = require "lspconfig"
-    local icons = require "benix.icons"
+  local wk = require "which-key"
+  wk.register {
+    name = "LSP",
+    ["<leader>la"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    ["<leader>lf"] = {
+      "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name = 'typescript-tools' end})<cr>",
+      "Format",
+    },
+    ["<leader>li"] = { "<cmd>LspInfo<cr>", "Info" },
+    ["<leader>lj"] = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
+    ["<leader>lh"] = { "<cmd>lua require('ben.lspconfig').toggle_inlay_hints()<cr>", "Hints" },
+    ["<leader>lk"] = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
+    ["<leader>ll"] = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+    ["<leader>lq"] = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+    ["<leader>lr"] = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+  }
 
-    local servers = {
-        "clangd",
-        "cmake",
-        "autotools_ls",
-        "lua_ls",
-        "cssls",
-        "html",
-        "tsserver",
-        "pyright",
-        "bashls",
-        "jsonls",
-        "jdtls",
-        "sqls",
-        "zls",
-        "rust_analyzer",
-        "gopls",
-        "nil_ls",
-    }
+  local lspconfig = require "lspconfig"
+  local icons = require "benix.icons"
 
-    local default_diagnostic_config = {
-        signs = {
-            active = true,
-            values = {
-                { name = "DiagnosticSignError", text = icons.diagnostics.Error },
-                { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-                { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-                { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-            }
-        }
-    }
+  local servers = {
+    "clangd",
+    "cmake",
+    "autotools_ls",
+    "lua_ls",
+    "cssls",
+    "html",
+    "tsserver",
+    "pyright",
+    "bashls",
+    "jsonls",
+    "jdtls",
+    "sqls",
+    "zls",
+    "rust_analyzer",
+    "gopls",
+    "nil_ls",
+  }
+
+  local default_diagnostic_config = {
+      signs = {
+          active = true,
+          values = {
+              { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+              { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+              { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+              { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
+          }
+      }
+  }
 
     vim.diagnostic.config(default_diagnostic_config)
 
@@ -65,6 +90,10 @@ function M.config()
         local require_ok, settings = pcall(require, "benix.lspsettings." .. server)
         if require_ok then
             opts = vim.tbl_deep_extend("force", settings, opts)
+        end
+
+        if server == "lua_ls" then
+          require("neodev").setup {}
         end
 
         lspconfig[server].setup(opts)

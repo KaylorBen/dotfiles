07f38664bc28445546e0081d241d872be287e368
Benjamin Kaylor <blkaylor22@gmail.com>
2024-03-19 09:18:49 -0600

    neovim config moved to lua

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,23 +1,13 @@
 {
-  imports = [
-    ./options.nix
-    ./keymaps.nix
-    ./plugins
-    # ./autoCmd.nix
-  ];
-
-  programs = { 
-    nixvim = {
-      enable = true;
-
-      colorschemes.gruvbox.enable = true;
-
-      enableMan = true;
-
-      globals = {
-        mapleader = " ";
-        maplocalleader = " ";
-      };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  home.file.".config/neovim/init.lua".source = "./init.lua";
+  home.file = {
+    ".config/neovim/lua" = {
+      source = ./lua;
+      recursive = true;
     };
   };
 }
---
+++ home/neovim/defaultOld.nix
@@ -0,0 +1,23 @@
+{
+  imports = [
+    ./options.nix
+    ./keymaps.nix
+    ./plugins
+    # ./autoCmd.nix
+  ];
+
+  programs = { 
+    nixvim = {
+      enable = true;
+
+      colorschemes.gruvbox.enable = true;
+
+      enableMan = true;
+
+      globals = {
+        mapleader = " ";
+        maplocalleader = " ";
+      };
+    };
+  };
+}

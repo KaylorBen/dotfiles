c1b7b614b8331d01f44e389d615ba29b42892d47
Benjamin Kaylor <blkaylor22@gmail.com>
2024-03-20 14:57:50 -0600

    reseting git

--- devshells/clang/flake.nix
+++ devshells/clang/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Java development environment";
+  description = "Clang development environment";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";

74665f6deb4a033f8a0fb6ed7512cb4019a7952e
Benjamin Kaylor <blkaylor22@gmail.com>
2024-03-26 16:02:24 -0600

    nvim nu integration

--- home/neovim/lua/benix/treesitter.lua
+++ home/neovim/lua/benix/treesitter.lua
@@ -10,7 +10,7 @@ local M = {
 
 function M.config()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "c", "cpp", "cmake", "make", "lua", "markdown", "markdown_inline", "bash", "python", "java", "zig", "rust", "go", "nix" }, -- put the language you want in this array
+    ensure_installed = { "c", "cpp", "cmake", "make", "lua", "markdown", "markdown_inline", "bash", "python", "java", "zig", "rust", "go", "nix", "nu"}, -- put the language you want in this array
     highlight = { enable = true },
     indent = { enable = true },
     refactor = {

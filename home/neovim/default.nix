c9dfa29d5deed54790584ed03b765606e3185bf3
Benjamin Kaylor <blkaylor22@gmail.com>
2024-03-30 19:18:49 -0600

    formatting

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -12,10 +12,8 @@
     ];
   };
   home.file.".config/nvim/init.lua".source = ./init.lua;
-  home.file = {
-    ".config/nvim/lua" = {
-      source = ./lua;
-      recursive = true;
-    };
+  home.file.".config/nvim/lua" = {
+    source = ./lua;
+    recursive = true;
   };
 }
--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -4,13 +4,6 @@
       enable = true;
       configFile.source = ./config.nu;
       envFile.source = ./env.nu;
-      environmentVariables = {
-        MANROFFOPT = "-c";
-        MANPAGER = "\"sh -c 'col -bx | bat -l man -p'\"";
-        BAT_THEME = "gruvbox-dark";
-        WINE = "/etc/profiles/per-user/ben/bin/wine";
-        WINETRICKS = "/etc/profiles/per-user/ben/bin/winetricks";
-      };
       shellAliases = {
         "l" = "ls -l";
         "la" = "ls -a";

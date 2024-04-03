cabadf05ab62e9198b2329cc0a4f71ad9b0483a8
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-03 17:57:34 -0600

    home/hypr

---
+++ modules/home/hyprland/default.nix
@@ -0,0 +1,240 @@
+{ config, inputs, lib, pkgs, osconfig, ... }:
+let cfg = config.tm.desktop.hyprland;
+in {
+  options.Wotan.desktop.hyprland = {
+    enable = lib.mkoption {
+      type = lib.types.bool;
+      description = "hyprland";
+      default = osconfig.Wotan.desktop.hyprland.enable or false;
+    };
+    extraautostart = lib.mkoption {
+      # list of strings
+      type = with lib.types; listof str;
+      default = if builtins.hasattr "tm" osconfig then
+        osconfig.Wotan.desktop.hyprland.extraautostart
+      else
+        [ ];
+    };
+    extrasettings = lib.mkoption {
+      type = lib.types.attrs;
+      default = if builtins.hasattr "Wotan" osconfig then
+        osconfig.Wotan.desktop.hyprland.extrasettings
+      else
+        { };
+    };
+  };
+
+  config = lib.mkif cfg.enable {
+    Wotan.programs.waybar.enable = true;
+    programs.rofi = {
+      enable = true;
+      package = pkgs.wofi;
+    };
+    services = {
+      mako.enable = true;
+      playerctld.enable = true;
+      swayidle = {
+        enable = true;
+        events = [
+          {
+            event = "before-sleep";
+            command = "${pkgs.swaylock-effects}/bin/swaylock";
+          }
+          {
+            event = "after-resume";
+            command = "${
+                lib.getexe' config.wayland.windowmanager.hyprland.package "hyprctl"
+              } dispatch dpms on";
+          }
+          {
+            event = "lock";
+            command = "${pkgs.swaylock-effects}/bin/swaylock";
+          }
+        ];
+        timeouts = [{
+          timeout = 300;
+          command = "${pkgs.systemd}/bin/loginctl lock-session";
+        }];
+      };
+    };
+
+    home.packages = with pkgs; [
+      rofi-wayland
+      gnome.gnome-keyring
+      gnome.seahorse
+      linux-wallpaperengine
+      steam
+      pamixer
+      inputs.hyprwm-contrib.packages.${pkgs.system}.grimblast
+      gnome.nautilus
+      networkmanager
+    ];
+
+    programs.swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        ignore-empty-password = true;
+        # Not using stylix
+        # font = config.stylix.fonts.monospace.name;
+        clock = true;
+        timestr = "%r";
+        datestr = "%a, %d %b";
+        fade-in = 0.2;
+        effect-blur = "20x2";
+        indicator = true;
+        indicator-radius = 240;
+        indicator-thickness = 20;
+        indicator-caps-lock = true;
+
+        disable-caps-lock-text = true;
+
+        inside-color = lib.mkforce "00000000";
+      };
+    };
+
+    wayland.windowmanager.hyprland = let
+      self = config.wayland.windowmanager.hyprland;
+    in {
+      enable = true;
+      xwayland = { enable = true; };
+      systemd.enable = true;
+      settings = {
+        source = "${./keybinds.conf}";
+        "$mainmod" = "super";
+        input = {
+          kb_layout = "us";
+          kb_options = "caps:escape";
+          touchpad = {
+            natural_scroll = true;
+            clickfinger_behavior = true;
+          };
+          follow_mouse = 1;
+          float_switch_override_focus = 2;
+          sensitivity = 0; # -1.0 to 1.0
+        };
+        gestures = {
+          workspace_swipe = true;
+          workspace_swipe_fingers = 4;
+          workspace_swipe_distance = 250;
+          workspace_swipe_invert = true;
+          workspace_swipe_min_speed_to_force = 15;
+          workspace_swipe_cancel_ratio = 0.5;
+          workspace_swipe_create_new = true;
+        };
+        env = [
+          "gdk_scale,2"
+          # Not using stylix
+          # "xcursor_size,${lib.tostring config.stylix.cursor.size}"
+        ];
+        xwayland = { force_zero_scaling = true; };
+        misc = {
+          disable_autoreload = true;
+          disable_hyprland_logo = true;
+          vrr = 2;
+          focus_on_activate = true;
+          always_follow_on_dnd = true;
+          layers_hog_keyboard_focus = true;
+          animate_manual_resizes = false;
+        };
+        general = {
+          gaps_in = lib.mkdefault 5;
+          gaps_out = lib.mkdefault 8;
+          border_size = lib.mkdefault 3;
+          layout = "dwindle"; # master | dwindle
+        };
+        dwindle = {
+          no_gaps_when_only = false;
+          force_split = 0;
+          special_scale_factor = 0.8;
+          split_width_multiplier = 1.0;
+          use_active_for_splits = true;
+          pseudotile = true;
+          preserve_split = true;
+        };
+        decoration = {
+          active_opacity = 1.0;
+          inactive_opacity = 0.85;
+          rounding = 10;
+
+          blur = {
+            size = 3;
+            passes = 1;
+            new_optimizations = true;
+          };
+          drop_shadow = true;
+          shadow_range = 4;
+          shadow_render_power = 3;
+          shadow_ignore_window = true;
+
+          dim_inactive = false;
+          dim_strength = 0.3;
+        };
+        animations = {
+          enabled = true;
+          bezier = "overshot, 0.13, 0.99, 0.29, 1.1";
+          animation = [
+            "windows, 1, 4, overshot, slide"
+            "windowsout, 1, 5, default, popin 80%"
+            "border, 1, 5, default"
+            "fade, 1, 8, default"
+            "workspaces, 1, 6, overshot, slidevert"
+          ];
+        };
+        # exec-once = [
+        #   "mako &"
+        #   "hyprctl setcursor '${config.stylix.cursor.name}' ${
+        #     tostring config.stylix.cursor.size
+        #   }"
+        #   "${pkgs.swaybg}/bin/swaybg -i ${config.stylix.image} -m fill"
+        #   "border_color &"
+        #   "${pkgs.networkmanagerapplet}/bin/nm-applet --indicator &"
+        #   "${wallpaper-engine} &"
+        # ] ++ cfg.extraautostart;
+        # `hyprctl clients` get class、title...
+        windowrule = [
+          "float,title:^(picture-in-picture)$"
+          "size 960 540,title:^(picture-in-picture)$"
+          "move 25%-,title:^(picture-in-picture)$"
+          "float,imv"
+          "move 25%-,imv"
+          "size 960 540,imv"
+          "float,mpv"
+          "move 25%-,mpv"
+          "size 960 540,mpv"
+          "float,danmufloat"
+          "move 25%-,danmufloat"
+          "pin,danmufloat"
+          "rounding 5,danmufloat"
+          "size 960 540,danmufloat"
+          "float,termfloat"
+          "move 25%-,termfloat"
+          "size 960 540,termfloat"
+          "rounding 5,termfloat"
+          "float,nemo"
+          "move 25%-,nemo"
+          "size 960 540,nemo"
+          "opacity 0.95,title:telegram"
+          "animation slide right,kitty"
+          "float,ncmpcpp"
+          "move 25%-,ncmpcpp"
+          "size 960 540,ncmpcpp"
+          "noblur,^(firefox)$"
+        ];
+        windowrulev2 = [
+          "opacity 0.0 override 0.0 override,class:^(xwaylandvideobridge)$"
+          "noanim,class:^(xwaylandvideobridge)$"
+          "nofocus,class:^(xwaylandvideobridge)$"
+          "noinitialfocus,class:^(xwaylandvideobridge)$"
+          "opacity 0.0 override 0.0 override,class:ffxiv_dx11.exe"
+          "noblur, class:ffxiv_dx11.exe"
+        ];
+      } // (import ./keybinds.nix {
+        inherit lib;
+        inherit pkgs;
+        inherit (self.settings.general) gaps_in;
+        inherit (self.settings.general) gaps_out;
+      }) // cfg.extrasettings;
+    };
+  };
+}
---
+++ modules/home/hyprland/keybinds.nix
@@ -0,0 +1,100 @@
+{ pkgs, gaps_out, gaps_in, ... }: {
+  bind = [
+    "$mainMod, Return, exec, ${pkgs.wezterm}/bin/wezterm"
+    "$mainMod SHIFT, Return, exec, wezterm --class='termfloat'"
+    "$mainMod SHIFT, Q, killactive"
+    "$mainMod SHIFT, E, exit"
+    "$mainMod SHIFT, SPACE, togglefloating"
+    "$mainMod SHIFT, SPACE, centerwindow"
+
+    "$mainMod, F, fullscreen"
+    "$mainMod, y, pin"
+    "$mainMod, P, pseudo"
+    "$mainMod, S, togglesplit"
+
+    # Grouped layout
+    "$mainMod, B, togglegroup"
+    "$mainMod, Tab, changegroupactive, f"
+    # Gap adjustment
+    "$mainMod SHIFT, G,exec,hyprctl --batch 'keyword general:gaps_out ${
+      builtins.toString gaps_out
+    };keyword general:gaps_in ${builtins.toString gaps_in}'"
+    "$mainMod , G,exec,hyprctl --batch 'keyword general:gaps_out 0;keyword general:gaps_in 0'"
+    # Move focus with mainMod + arrow keys
+    "$mainMod, H, movefocus, l"
+    "$mainMod, J, movefocus, d"
+    "$mainMod, K, movefocus, u"
+    "$mainMod, L, movefocus, r"
+    # Switch workspaces with mainMod + [0-9]
+    "$mainMod, 1, workspace, 1"
+    "$mainMod, 2, workspace, 2"
+    "$mainMod, 3, workspace, 3"
+    "$mainMod, 4, workspace, 4"
+    "$mainMod, 5, workspace, 5"
+    "$mainMod, 6, workspace, 6"
+    "$mainMod, 7, workspace, 7"
+    "$mainMod, 8, workspace, 8"
+    "$mainMod, 9, workspace, 9"
+    "$mainMod, 0, workspace, 10"
+    "$mainMod, right, workspace, +1"
+    "$mainMod, left, workspace, -1"
+    "$mainMod, period, workspace, e+1"
+    "$mainMod, comma, workspace,e-1"
+
+    "$mainMod SHIFT, H ,movewindow, l"
+    "$mainMod SHIFT, J ,movewindow, d"
+    "$mainMod SHIFT, K ,movewindow, u"
+    "$mainMod SHIFT, L ,movewindow, r"
+
+    "$mainMod CTRL, 1, movetoworkspace, 1"
+    "$mainMod CTRL, 2, movetoworkspace, 2"
+    "$mainMod CTRL, 3, movetoworkspace, 3"
+    "$mainMod CTRL, 4, movetoworkspace, 4"
+    "$mainMod CTRL, 5, movetoworkspace, 5"
+    "$mainMod CTRL, 6, movetoworkspace, 6"
+    "$mainMod CTRL, 7, movetoworkspace, 7"
+    "$mainMod CTRL, 8, movetoworkspace, 8"
+    "$mainMod CTRL, 9, movetoworkspace, 9"
+    "$mainMod CTRL, 0, movetoworkspace, 10"
+    "$mainMod CTRL, left, movetoworkspace, -1"
+    "$mainMod CTRL, right, movetoworkspace, +1"
+    # same as above, but doesnt switch to the workspace
+    "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+    "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+    "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+    "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+    "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+    "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+    "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+    "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+    "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+    "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+    # Scroll through existing workspaces with mainMod + scroll
+    "$mainMod, mouse_down, workspace, e+1"
+    "$mainMod, mouse_up, workspace, e-1"
+
+    "$mainMod,slash,workspace,previous"
+
+    "$mainMod SHIFT,X,exec,swaylock"
+    "$mainMod, bracketleft,exec,grimblast --notify --cursor  copysave area ~/Pictures/$(date '+%Y-%m-%d'T'%H:%M:%S_no_watermark').png"
+    "$mainMod, bracketright,exec, grimblast --notify --cursor  copy area"
+    "$mainMod, A,exec, grimblast_watermark"
+    ",Super_L, exec, pkill rofi || ~/.config/rofi/launcher.sh"
+    "$mainMod, Super_L,exec, bash ~/.config/rofi/powermenu.sh"
+    "$mainMod, SPACE, exec, rofi -show drun"
+
+    # control volume,brightness,media players
+    ", XF86AudioRaiseVolume,exec, ${pkgs.wireplumber}/bin/wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
+    ", XF86AudioLowerVolume,exec, ${pkgs.wireplumber}/bin/wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-"
+    ", XF86AudioMute,exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+    ", XF86AudioMicMute,exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle "
+    ", XF86MonBrightnessUp,exec, ${pkgs.light}/bin/light -A 5"
+    ", XF86MonBrightnessDown, exec, ${pkgs.light}/bin/light -U 5"
+    ", XF86AudioPlay,exec, ${pkgs.playerctl}/bin/playerctl play-pause"
+    ", XF86AudioNext,exec, ${pkgs.playerctl}/bin/playerctl next"
+    ", XF86AudioPrev,exec, ${pkgs.playerctl}/bin/playerctl previous"
+
+    # Toggle waybar
+    "$mainMod, O, exec, killall -SIGUSR1 .waybar-wrapped"
+  ];
+}

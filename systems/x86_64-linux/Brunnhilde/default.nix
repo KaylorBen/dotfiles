afd1c0df1f9599297eda7dc88fd2d888c1903664
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-03 04:42:18 -0600

    laptop setup init

---
+++ systems/x86_64-linux/Brunnhilde/default.nix
@@ -0,0 +1,73 @@
+{ pkgs, ... }:
+{
+  imports = [ ./hardware-configuration.nix ];
+  Wotan = {
+    autoUpgrade = true;
+    zfs.enable = true;
+    users.enable = true;
+    time.enable = true;
+    impermanence = {
+      enable = true;
+      rollbackCommand = ''
+        zfs rollback -r zroot/encrypted/NixOS/root@blank
+      '';
+    };
+    sound.enable = true;
+    secutiy = {
+      enableTPM = false;
+      enableSecureBoot = true;
+    };
+    desktop = {
+      hyprland.enable = true;
+    };
+  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # Plymouth
+  boot.plymouth.enable = false;
+
+  programs = {
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+    kdeconnect.enable = true;
+    mosh.enable = true;
+    noisetorch.enable = true;
+    partition-manager.enable = true;
+  };
+  networking = {
+    networkmanager.enable = true;
+    useDHCP = false;
+    interfaces.wlp2s0.useDHCP = true;
+  };
+  services = {
+    xserver = {
+      enable = true;
+
+      # Configure keymap in X11
+      layout = "us";
+
+      # Enable touchpad support
+      libinput.enable = true;
+    };
+
+    openssh.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    git
+    nil
+  ];
+
+  security.polkit.enable = true;
+
+  snowfallorg.users.ben.home.config = {
+    Wotan.home-profiles.desktop.enable = true;
+  };
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+}
--- systems/x86_64-linux/Brunnhilde/hardware-configuration.nix
+++ systems/x86_64-linux/Brunnhilde/hardware-configuration.nix
@@ -1,8 +1,10 @@
 { config, lib, pkgs, modulesPath, inputs, ... }:
-
 {
   imports = with inputs; [ 
     (modulesPath + "/installer/scan/not-detected.nix")
+    nixos-hardware.nixosModules.common-cpu-intel
+    nixos-hardware.nixosModules.common-pc-laptop-ssd
+    nixos-hardware.nixosModules.common-pc-laptop
     nixos-hardware.nixosModules.dell-xps-13-9310
   ];
 
@@ -10,20 +12,26 @@
     initrd = {
       systemd.enable = true;
       availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+      binfmt.emulatedSystems = [ "aarch64-linux" "riscv32-linux" "riscv64-linux" "x86_64-windows" ];
       kernelModules = [ ];
     };
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
   };
-
+  services = {
+    fwupd.enable = true;
+  };
+  networking = {
+    hostName = "Brunnhilde";
+    hostId = "7c40a3b8";
+  };
   Wotan = {
     zfs.enable = true;
     laptop = lib.mkDefault true;
   };
 
-  hardware.nvidia.modesetting.enable = true;
-  
   disko.devices = import ./disko.nix { inherit lib; };
+  fileSystems."/.persistent".neededForBoot = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -33,5 +41,6 @@
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

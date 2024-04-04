e1e885ba6bbfa2ebe055153b2d1df775707974a1
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-03 20:37:41 -0600

    fixed script up a bit

--- build-iso-script.sh
+++ build-iso-script.sh
@@ -1,6 +1,5 @@
 mkdir -p result
-export ISO_PATH=$(nix --extra-experimental-features 'nix-command' --extra-experimental-features 'flakes' build .\#install-isoConfigurations.gunther --no-link --print-out-paths -L)
-export ISO_NAME=$(nix --extra-experimental-features 'nix-command' --extra-experimental-features 'flakes' eval .\#install-isoConfigurations.gunther.isoName --raw)
+export ISO_PATH=$(nix --extra-experimental-features 'nix-command' --extra-experimental-features 'flakes' build .\#install-isoConfigurations.Gunther --no-link --print-out-paths -L)
+export ISO_NAME=$(nix --extra-experimental-features 'nix-command' --extra-experimental-features 'flakes' eval .\#install-isoConfigurations.Gunther.isoName --raw)
 
 cp -rv "$ISO_PATH/iso/$ISO_NAME" "result/$ISO_NAME"
-

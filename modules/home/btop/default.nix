72e61f19e827857956231f1ec14919ab928ec324
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-08 09:23:00 -0600

    btop

---
+++ modules/home/btop/default.nix
@@ -0,0 +1,19 @@
+{ lib, config, ... }:
+{
+  options.Wotan.programs.btop = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Htop but better";
+      # Enable btop by default
+      default = true;
+    };
+  };
+
+  config = lib.mkIf config.Wotan.programs.btop.enable {
+    home.file.".config/btop/themes/rose-pine.theme".source = ./rose-pine.theme;
+    programs.btop = {
+      enable = true;
+      color_theme = "rose-pine";
+    };
+  };
+}

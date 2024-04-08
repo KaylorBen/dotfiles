97a74f9246a45f4e9526ba04771a39bf2c1b61c8
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-08 06:53:08 -0600

    starship options

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -7,6 +7,37 @@
   config = lib.mkIf config.Wotan.programs.starship.enable {
     programs.starship = {
       enable = true;
+      settings = {
+        add_newline = lib.mkDefault false;
+        battery = lib.mkDefault {
+          full_symbol = "🔋";
+          charging_symbol = "⚡️";
+          discharging_symbol = "💀";
+          display = [
+            {
+              threshold = 10;
+              style = "bold red";
+            }
+            {
+              threshold = 30;
+              style = "bold yellow";
+            }
+          ];
+        };
+        username = {
+          show_always = lib.mkDefault true;
+          style_user = lib.mkDefault "bold green";
+          style_root = lib.mkDefault "bold red";
+        };
+        directory = {
+          substitutions = {
+            "Documents" = "󰈙 ";
+            "Downloads" = " ";
+            "Music" = " ";
+            "Pictures" = " ";
+          };
+        };
+      };
       enableNushellIntegration = true;
     };
 

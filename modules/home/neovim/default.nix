1220a7312d3d8d6541bdf3b7d1bcd5d9c5dc570a
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-08 03:01:53 -0600

    neovim default

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -2,7 +2,12 @@
 let cfg = config.Wotan.programs.neovim;
 in {
   options.Wotan.programs.neovim = {
-    enable = lib.mkEnableOption "Enable neovim";
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Enable neovim";
+      # Enable neovim by default
+      default = true;
+    };
     defaultEditor = lib.mkOption {
       type = lib.types.bool;
       description = "Use neovim as the default editor";
@@ -13,6 +18,7 @@ in {
   config = lib.mkIf cfg.enable {
     programs.neovim = {
       enable = true;
+      package = pkgs.neovim;
       defaultEditor = cfg.defaultEditor;
     };
     home.file.".config/nvim" = {

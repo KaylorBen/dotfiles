6618971c270bbcd35af7236175826745e94fbaf8
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-09 06:41:55 -0600

    harpoon on

--- modules/home/neovim/nvim/lua/benix/harpoon.lua
+++ modules/home/neovim/nvim/lua/benix/harpoon.lua
@@ -5,20 +5,20 @@ local M = {
 function M.config()
   local wk = require("which-key")
   wk.register {
-    ["<leader>Hm"] = { "<cmd>lua require('harpoon.mark').add_file()", "Mark File" },
-    ["<leader>Hv"] = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()", "Toggle Menu" },
-    ["<leader>Hn"] = { "<cmd>lua require('harpoon.ui').nav_next()", "Next File" },
-    ["<leader>Hp"] = { "<cmd>lua require('harpoon.ui').nav_prev()", "Previous File" },
+    ["<leader>Hm"] = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Mark File" },
+    ["<leader>Hv"] = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle Menu" },
+    ["<leader>Hn"] = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "Next File" },
+    ["<leader>Hp"] = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "Previous File" },
 
-    ["<leader>H1"] = { "<cmd>lua require('harpoon.ui').nav_file(1)", "Jump to File 1" },
-    ["<leader>H2"] = { "<cmd>lua require('harpoon.ui').nav_file(2)", "Jump to File 2" },
-    ["<leader>H3"] = { "<cmd>lua require('harpoon.ui').nav_file(3)", "Jump to File 3" },
-    ["<leader>H4"] = { "<cmd>lua require('harpoon.ui').nav_file(4)", "Jump to File 4" },
-    ["<leader>H5"] = { "<cmd>lua require('harpoon.ui').nav_file(5)", "Jump to File 5" },
-    ["<leader>H6"] = { "<cmd>lua require('harpoon.ui').nav_file(6)", "Jump to File 6" },
-    ["<leader>H7"] = { "<cmd>lua require('harpoon.ui').nav_file(7)", "Jump to File 7" },
-    ["<leader>H8"] = { "<cmd>lua require('harpoon.ui').nav_file(8)", "Jump to File 8" },
-    ["<leader>H9"] = { "<cmd>lua require('harpoon.ui').nav_file(9)", "Jump to File 9" },
+    ["<leader>H1"] = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Jump to File 1" },
+    ["<leader>H2"] = { "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "Jump to File 2" },
+    ["<leader>H3"] = { "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "Jump to File 3" },
+    ["<leader>H4"] = { "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "Jump to File 4" },
+    ["<leader>H5"] = { "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", "Jump to File 5" },
+    ["<leader>H6"] = { "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", "Jump to File 6" },
+    ["<leader>H7"] = { "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", "Jump to File 7" },
+    ["<leader>H8"] = { "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", "Jump to File 8" },
+    ["<leader>H9"] = { "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", "Jump to File 9" },
   }
 
   require("harpoon").setup({

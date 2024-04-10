f300c5c525b2c212b444fdf807016511f81a44bf
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-10 01:31:23 -0600

    picom

---
+++ modules/home/picom/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, osConfig, ... }:
+{
+  options.Wotan.desktop.picom = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      description = "Picom compositor toggle";
+      default = osConfig.Wotan.desktop.awesome.enable or false;
+    };
+  };
+  config = lib.mkIf config.Wotan.desktop.picom.enable {
+    services.picom = {
+      enable = true;
+      activeOpacity = 1.0;
+      fade = true;
+      fadeDelta = 0.05;
+      inactiveOpacity = 0.8;
+      shadow = true;
+    };
+  };
+}

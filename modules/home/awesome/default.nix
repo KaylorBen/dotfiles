7754c351c2b513fbdae81e48811fc6d43609ce10
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-10 03:41:07 -0600

    flameshot

--- modules/home/awesome/default.nix
+++ modules/home/awesome/default.nix
@@ -12,9 +12,22 @@
       source = ./awesome;
       recursive = true;
     };
-    services.playerctld.enable = true;
+    services = {
+      playerctld.enable = true;
+      flameshot = {
+        enable = true;
+        settings = {
+          General = {
+            uiColor = "#1f1d2e";
+            contrastUiColor = "#6e6a86";
+            drawColor = "#eb6f92";
+          };
+        };
+      };
+    };
 
     home.packages = with pkgs; [
+      xorg.libxcb
       pamixer
       pa_applet
       xclip
--- modules/home/wezterm/wezterm/wezterm.lua
+++ modules/home/wezterm/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'rose-pine';
 config.hide_tab_bar_if_only_one_tab = true;
 config.enable_wayland = false;
+config.font = wezterm.font('Fantasque Sans Mono')
 
 -- and finally, return the configuration to wezterm
 return config

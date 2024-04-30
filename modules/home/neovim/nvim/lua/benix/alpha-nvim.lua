f9b14529a2c7c3268f9c49d2cba4c03761c9ded1
Benjamin Kaylor <blkaylor22@gmail.com>
2024-04-30 15:46:42 -0600

    alpha-nvim

---
+++ modules/home/neovim/nvim/lua/benix/alpha-nvim.lua
@@ -0,0 +1,13 @@
+local M = {
+  "goolord/alpha-nvim",
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'nvim-lua/plenary.nvim'
+  },
+}
+
+function M.config()
+  require'alpha'.setup(require'alpha.themes.dashboard'.config)
+end
+
+return M

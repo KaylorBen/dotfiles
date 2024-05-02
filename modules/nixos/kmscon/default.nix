adccd14ff1edde5ef437ff81bb99be615361a25c
Benjamin Kaylor <blkaylor22@gmail.com>
2024-05-02 14:06:13 -0600

    added kmscon, perf issues though

---
+++ modules/nixos/kmscon/default.nix
@@ -0,0 +1,22 @@
+{config, lib, pkgs, ...}:
+let
+  cfg = config.Wotan.kmscon;
+in {
+  options.Wotan.kmscon = {
+    enable = lib.mkEnableOption "kmscon TTY interface";
+    fonts = lib.mkOption {
+      type = with lib.types; listOf attrs;
+      default = [ {
+        name = "FiraMono Nerd Font";
+        package = pkgs.nerdfonts;
+      } ];
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.kmscon = {
+      enable = true;
+      fonts = cfg.fonts;
+    };
+  };
+}

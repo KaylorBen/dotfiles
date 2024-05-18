69f7342d62a49bfcaf34232a487ef62462f731ce
Benjamin Kaylor <blkaylor22@gmail.com>
2024-05-18 19:46:48 -0400

    treesitter

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -11,8 +11,8 @@ _prev: super: {
   inherit (nix-citizen.packages.${super.system})
     lug-helper star-citizen-helper star-citizen;
   # wezterm = wezterm.packages.${super.system}.default;
-  hyprland = hyprland.packages.${super.system}.hyprland;
-  xdg-desktop-portal-hyprland = xdg-desktop-portal-hyprland.packages.${super.system}.xdg-desktop-portal-hyprland;
+  # hyprland = hyprland.packages.${super.system}.hyprland;
+  # xdg-desktop-portal-hyprland = xdg-desktop-portal-hyprland.packages.${super.system}.xdg-desktop-portal-hyprland;
   picom = picom.defaultPackage.${super.system};
 
   lutris = super.lutris.override {

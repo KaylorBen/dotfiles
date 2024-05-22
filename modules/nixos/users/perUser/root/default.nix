be974bd34bbe9ebaba866b5e9f91fb47ae197e4e
Benjamin Kaylor <blkaylor22@gmail.com>
2024-05-22 18:35:27 -0400

    temp password disabled

--- modules/nixos/users/perUser/root/default.nix
+++ modules/nixos/users/perUser/root/default.nix
@@ -5,7 +5,7 @@
     snowfallorg.users.root.create = false;
     users.users.root = {
       openssh.authorizedKeys.keyFiles = lib.Wotan.get-ssh-key-files "root";
-      initialPassword = "NixOS4Life";
+      # initialPassword = "NixOS4Life";
       extraGroups = [ "ssh" ];
     };
   };

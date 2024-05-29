3b8007968b2f10a9f53559744c25baa73e893b93
Benjamin Kaylor <blkaylor22@gmail.com>
2024-05-29 15:32:52 -0400

    picom + printing + monitor change

--- systems/x86_64-linux/Siegmund/default.nix
+++ systems/x86_64-linux/Siegmund/default.nix
@@ -70,6 +70,12 @@
     };
     # dbus.enable = true;
     openssh.enable = true;
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
 
   users.users.ben.packages = with pkgs; [

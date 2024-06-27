9cc8e0449c187229ade5a6a8bc74737d5f8f99b9
Benjamin Kaylor <blkaylor22@gmail.com>
2024-06-27 01:56:21 -0400

    vencord back!

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -9,10 +9,10 @@ _prev: super: {
   # xdg-desktop-portal-hyprland = xdg-desktop-portal-hyprland.packages.${super.system}.xdg-desktop-portal-hyprland;
   picom = picom.defaultPackage.${super.system};
 
-  # discord = super.discord.override {
-  #   withOpenASAR = true;
-  #   withVencord = true;
-  # };
+  discord = super.discord.override {
+    withOpenASAR = true;
+    withVencord = true;
+  };
 
   lutris = super.lutris.override {
     steamSupport = true;
